05d70fdcec10f125be36f01f21527906f0fd5601
Prateek Narula <prateeknarula@prateeks-mbp.localdomain>
2018-01-04 16:47:27 +0100

    First Commit

---
+++ fbquizapp/quiz-mania-app/src/components/FBAuthetication.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ImageTemplate extends Component {
+  render() {
+    return (
+      <div className="App">
+        <table>
+          <tr>
+            <td>
+              <p>This is just a trial. This is just a trial. This is just a trial.</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>This is just a trial 2.</p>
+            </td>
+            <td>
+              <p>This is just a trial 3.</p>
+            </td>
+            <td>
+              <p>This is just a trial 4.</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>This is just a trial. This is just a trial. This is just a trial.</p>
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default ImageTemplate;
